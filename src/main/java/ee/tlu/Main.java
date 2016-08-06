763f20eab11ad36dc489c4e4f8251f58527f9195
ktenman <ktenman@users.noreply.github.com>
2016-08-06 21:12:59 +0300

    Lahendus.

--- src/main/java/ee/tlu/Main.java
+++ src/main/java/ee/tlu/Main.java
@@ -1,9 +1,27 @@
 package ee.tlu;
 
-public class Main {
+class Main {
 
-    public String convertWord(String word) {
-        StringBuilder answer = new StringBuilder();
-        return answer.toString();
+    String convertWord(String word) {
+        String answer = "";
+        int counter = 1;
+        for (int i = 0; i < word.length(); i++) {
+            char currentLetter = word.charAt(i);
+            char nextLetter = 0;
+            try {
+                nextLetter = word.charAt(i + 1);
+            } catch (Exception e) {
+                System.out.println("Error: " + e);
+            }
+            boolean letterEqualsWithOneLetterBefore = currentLetter == nextLetter;
+            boolean notLastLetter = i != word.length() - 1;
+            if (letterEqualsWithOneLetterBefore && notLastLetter) {
+                counter++;
+            } else {
+                answer += counter + Character.toString(currentLetter);
+                counter = 1;
+            }
+        }
+        return answer;
     }
 }
