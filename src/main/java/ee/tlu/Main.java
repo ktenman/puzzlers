4f6b826f18d2a72b455c41ee0b64a33ee27a2c02
ktenman <ktenman@users.noreply.github.com>
2016-08-06 22:51:14 +0300

    Puzzle

--- src/main/java/ee/tlu/Main.java
+++ src/main/java/ee/tlu/Main.java
@@ -3,25 +3,43 @@ package ee.tlu;
 class Main {
 
     String convertWord(String word) {
-        String answer = "";
-        int counter = 1;
-        for (int i = 0; i < word.length(); i++) {
-            char currentLetter = word.charAt(i);
-            char nextLetter = 0;
-            try {
-                nextLetter = word.charAt(i + 1);
-            } catch (Exception e) {
-                System.out.println("Error: " + e);
+//        String answer = "";
+//        int counter = 1;
+//        for (int i = 0; i < word.length(); i++) {
+//            char currentLetter = word.charAt(i);
+//            char nextLetter = 0;
+//            try {
+//                nextLetter = word.charAt(i + 1);
+//            } catch (Exception e) {
+//                System.out.println("Error: " + e);
+//            }
+//            boolean letterEqualsWithOneLetterBefore = currentLetter == nextLetter;
+//            boolean notLastLetter = i != word.length() - 1;
+//            if (letterEqualsWithOneLetterBefore && notLastLetter) {
+//                counter++;
+//            } else {
+//                answer += counter + Character.toString(currentLetter);
+//                counter = 1;
+//            }
+//        }
+//        return answer;
+        String result = "";
+        StringBuilder sb = new StringBuilder(word);
+        while (sb.length() != 0) {
+            int count = 0;
+            char test = sb.charAt(0);
+            search:
+            {
+                while (sb.indexOf(String.valueOf(test)) != -1) {
+                    if (sb.indexOf(String.valueOf(test)) > 1) {
+                        break search;
+                    }
+                    sb.deleteCharAt(sb.indexOf(String.valueOf(test)));
+                    count++;
+                }
             }
-            boolean letterEqualsWithOneLetterBefore = currentLetter == nextLetter;
-            boolean notLastLetter = i != word.length() - 1;
-            if (letterEqualsWithOneLetterBefore && notLastLetter) {
-                counter++;
-            } else {
-                answer += counter + Character.toString(currentLetter);
-                counter = 1;
-            }
+            result = result + count + test;
         }
-        return answer;
+        return result;
     }
 }
